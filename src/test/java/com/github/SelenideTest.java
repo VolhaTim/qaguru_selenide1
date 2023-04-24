a91509d3ed063699105c0aa1271f488f7320f930
VolhaTim <volha.tim.87@gmail.com>
2023-04-25 01:49:07 +0300

    created test

--- src/test/java/com/github/SelenideTest.java
+++ src/test/java/com/github/SelenideTest.java
@@ -1,8 +1,28 @@
 package com.github;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class SelenideTest {
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.holdBrowserOpen=true;
+    }
+
     @Test
     void SuccessSelenideTest(){
+        open ("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $(".js-wiki-more-pages-link").click();
+        $(byTagAndText("a","SoftAssertions")).click();
+        $("#wiki-body").shouldHave(text("JUnit5"));
+
+    }
+
 }
